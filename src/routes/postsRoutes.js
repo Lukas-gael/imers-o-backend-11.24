960d1649985ee3ed9c913a3706f67b80e9671c7f
LucasGael <lukasgael.live@gmail.com>
2024-11-22 22:07:48 -0300

    commit routes

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controllers/postsController.js';
 
+const corsOption = {
+  origin: 'http/:localhost:8000',
+  optionsSuccessStatus: 200;
+}
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/'); 
@@ -16,6 +21,7 @@ const upload = multer({dest:'./uploads' , storage});
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsoOptions))
     app.get('/posts', listarPosts);
     app.post('/posts', postarNovoPost);
     app.post('/upload', upload.single("imagem"), uploadImagem);
